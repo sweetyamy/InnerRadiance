f341f5822442ec644907e4f61d4077e903eac0e7
Amy Ahn <amy.ahn.kr@gmail.com>
2024-08-23 21:40:24 -0400

    reduced db

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -4,17 +4,23 @@ import { useNavigate } from 'react-router-dom';
 const ProductCard = ({ item }) => {
   const navigate = useNavigate();
   const showDetail = (id) => {
-    navigate(`/product/${id}`);
+    navigate(`/${id}`);
   };
   return (
     <div onClick={() => showDetail(item.id)} className='product-card-container'>
       {item?.new === true && <div className='new-badge'>New</div>}
-      <img src={item?.img} alt='product images' />
-      <div className='product-card-typography'>
-        {item?.choice === true ? 'Conscious Choice' : ''}
+      <img src={item?.img} className='product-img' alt='product images' />
+      <div className='product-card-title'>
+        {item?.title.length > 14
+          ? `${item.title.substring(0, 10)}...`
+          : item.title}
       </div>
-      <div className='product-card-title'>{item?.title}</div>
-      <div className='product-card-typography'>{item?.price}</div>
+      <div className='product-card-typography'>
+        {item?.price.toLocaleString('en-US', {
+          style: 'currency',
+          currency: 'CAN'
+        })}
+      </div>
     </div>
   );
 };
